5d6928782486ed88028c5fc7edd33091ce99201a
Aurel Weinhold <aurel@weinhold.org>
2020-05-27 18:09:53 +0200

    Minor cleanup and todos

--- src/chip8.cpp
+++ src/chip8.cpp
@@ -29,30 +29,6 @@ int main(int argc, char *args[]) {
         std::exit(EXIT_FAILURE);
     }
 
-
-    // NOTE(aurel): Memory:
-    //   - 4096 Bytes = 4KB
-    //   - fist 512 Bytes reserved for the chip-8 interpreter itself -> most programs begin at memory
-    //     location 512 (0x200) --> Today common to store font data here.
-    //   - uppermost 256 bytes (0xF00 - 0xFFF) reserved for display refresh
-    //   - the 96 bytes below (0xEA0 - 0xEFF) reserved for the call stack, internal use, and other variables.
-
-    // NOTE(aurel): Registers:
-    //  - 16 8-bit registers named V0 - VF
-    //  - VF acts as a flag for some operations -> should be avoided
-    //  - address register, called I, is 16 bits wide and is used with several opcodes that involve memory
-    //    operations
-
-    // NOTE(aurel): Stack:
-    //  - only used to store return addresses when subroutines are called
-
-    // NOTE(aurel): Timers:
-    //  - Delay timer: intended to be used to time events of games
-    //  - Sound timer: used for sound effects. When its value is non-zero, a beeping sound is made.
-
-
-
-
     // TODO(aurel): Initialize memory
     // NOTE(aurel): Chars are 8-bit wide!
     char *memory = new (std::nothrow) char[4096];
@@ -82,6 +58,8 @@ int main(int argc, char *args[]) {
 
         std::string instr_str;
 
+        // TODO(aurel): Probably rewrite to only check the first 4 bit in the outermost switch
+        //  ((instruction & 0xf000) >> 3). This should make it more readable and avoid the ranged cases.
         switch (instruction) {
             case 0x0000 ... 0x0fff:
                 switch (instruction & 0x0fff){
